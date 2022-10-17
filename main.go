24901bbebe0e4a59f8d23ebdc48ac0c58621f74f
standchan <standchan@foxmail>
2022-10-17 20:22:23 +0800

    add signal gorountine

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "kafka-tui/core"
+import (
+	"fmt"
+	"kafka-tui/core"
+	"os"
+	"os/signal"
+)
 
 func main() {
 	// welcomeScreen := tview.NewTextView().SetBorder(true).SetTitle("Hello, world!")
@@ -14,4 +19,13 @@ func main() {
 	if err := core.NewKafkaTUI().Start(); err != nil {
 		panic(err)
 	}
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c)
+		for stop := range c {
+			fmt.Println("get exit signal", stop)
+			os.Exit(0)
+		}
+	}()
+	select {}
 }
